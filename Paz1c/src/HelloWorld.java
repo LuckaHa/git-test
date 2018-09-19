ab9138a25540d75ccfad3f895c755c2630c8a9dc
student <student@158.197.35.86>
2018-09-19 09:02:54 +0200

    For i

--- Paz1c/src/HelloWorld.java
+++ Paz1c/src/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 		System.out.println("Git je super.");
 		// komentar
 		//kometar Jakub
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("i:" + i);
+		}
 	}
 
 }
